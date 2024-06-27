1d4101056ca3dc4d69b37b2cbf86bfddc090e751
jasonronalddavis <jdavis@c.ringling.edu>
2024-06-27 18:16:26 -0500

    All crawl functions successful

--- peanut-app/src/controls/hexapodControl.js
+++ peanut-app/src/controls/hexapodControl.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import BluetoothService from '../Bluetooth/bluetoothServices';
 import './controller.css';
 import eyeBar from '../Images/eyeBar.png';
@@ -6,288 +6,198 @@ import eyeBarCopy from '../Images/eyeBarCopy.png';
 import eyeWindow from '../Images/eyeWindow.png';
 import FrontEyeBar from '../Images/FrontEyeBar.png';
 import mouthWindow from '../Images/mouthWindow.png';
+import crawl_forward from '../Images/crawl_forward.png';
+import crawl_backward from '../Images/crawl_backward.png';
+import crawl_right from '../Images/crawl_right.png';
+import crawl_left from '../Images/crawl_left.png';
+import stand_image from '../Images/stand.png';
+import squat_down from '../Images/squat_down.png';
+import body_window from '../Images/body_window.png';
+import blink_button from '../Images/blink_button.png';
+import mouth from '../Images/mouth.png';
+import stretch_image from '../Images/stretch_image.png';
 
-//crawlR
 const HexapodControl = ({ device }) => {
-  const [scrollbarValue, setScrollbarValue] = useState(0);
-  const [crawlForward, setCrawlForward] = useState(false); // State for tracking crawling forward action
-  const [crawlBackward, setCrawlBackward] = useState(false); // State for tracking crawling forward action
-  const [crawlRight, setCrawlRight] = useState(false); // State for tracking crawling forward action
-  const [width, setWidth] = useState(140); // Initial width of the rectangle
+  const [width, setWidth] = useState(140);
+  const [height, setHeight] = useState(100);
   const [dragging, setDragging] = useState(false);
   const [startX, setStartX] = useState(null);
   const [startY, setStartY] = useState(null);
-  const [height, setHeight] = useState(100); // Initial width of the rectangle
-  const minWidth = 20;
-  const maxWidth = 140;
-  const minHeight = 20;
-  const maxHeight = 140;
 
-
-  const xBarDown = (e) => {
-    setDragging(true);
-    setStartX(e.clientX); // Store the starting X coordinate
-  };
-
-  // Effect for adjusting bar length based on scrollbar value
-
-
-  const xBarUp = () => {
-    // Stop dragging when the mouse is released
-    setDragging(false);
-  };
-
-
-
-  const yBarDown = (e) => {
-    setDragging(true);
-    setStartY(e.clientY); // Store the starting X coordinate
-  };
-
-
-  const yBarUp = () => {
-    // Stop dragging when the mouse is released
-    setDragging(false);
-  };
-
-  const xBarMove = (e) => {
-    if (dragging) {
-      // Calculate the change in X coordinate
-      const deltaX = e.clientX - startX;
-      // Update the rectangle width based on the change
-      const newWidth = width + deltaX;
-      const clampedWidth = Math.min(Math.max(newWidth, minWidth), maxWidth);
-      setWidth(clampedWidth);
-
-      // Transmit the new width value via Bluetooth
-      if (device) {
-        BluetoothService.sendCommand(device, `x${clampedWidth}`);
-      }
-
-      setStartX(e.clientX);
+  const startCrawl = (direction) => {
+    if (device) {
+      BluetoothService.sendCommand(device, direction);
     }
   };
 
-
-  const yBarMove = (e) => {
-    if (dragging) {
-      // Calculate the change in X coordinate
-      const deltaY = e.clientY - startY;
-      // Update the rectangle width based on the change
-      const newHeight = height + deltaY;
-      const clampedHeight = Math.min(Math.max(newHeight, minHeight), maxHeight);
-      setHeight(clampedHeight);
-console.log(clampedHeight);
-      // Transmit the new width value via Bluetooth
-      if (device) {
-        BluetoothService.sendCommand(device, `y${clampedHeight}`);
-      }
-
-      setStartY(e.clientY);
+  const stopCrawl = (direction) => {
+    if (device) {
+      BluetoothService.sendCommand(device, `${direction}0`);
     }
-};
+  };
 
-  useEffect(() => {
-    const fIntervalId = setInterval(() => {
-      if (crawlForward && device) {
-        console.log('Crawling forward...');
-        BluetoothService.sendCommand(device, new Uint8Array([11])); // Send the command to start crawling forward
-      }
-    }, 20); // Adjust the interval as needed
+  const handleLeftStart = () => {
+    console.log('Crawling left...');
+    startCrawl('L');
+  };
 
-    const bIntervalId = setInterval(() => {
-      if (crawlBackward && device) {
-        console.log('Crawling backward...');
-        BluetoothService.sendCommand(device, new Uint8Array([11])); // Send the command to start crawling forward
-      }
-    }, 20);
-    const rIntervalId = setInterval(() => {
-      if (crawlRight && device) {
-        console.log('Crawling backward...');
-        BluetoothService.sendCommand(device, new Uint8Array([12])); // Send the command to start crawling forward
-      }
-    }, 20);
+  const handleLeftStop = () => {
+    console.log('Stopping crawl left...');
+    stopCrawl('L');
+  };
 
-    return () => {
-      clearInterval(fIntervalId); // Clear interval on component unmount
-      clearInterval(bIntervalId);
-      clearInterval(rIntervalId);
-    };
-  }, [crawlForward, device], [crawlBackward, device], [crawlRight, device]);
+  const handleRightStart = () => {
+    console.log('Crawling right...');
+    startCrawl('R');
+  };
 
-
-const handleRightStart = () =>{
-  console.log('Crawling backward...');
-  if (device) {
-    console.log('Transmitting command: 11');
-    const byteCommand = `X${12}`;
-    BluetoothService.sendCommand(device,byteCommand); // Send the command to start crawling forward
-    setCrawlRight(true); // Set the state to indicate crawling forward
-  }
-
-};
-
+  const handleRightStop = () => {
+    console.log('Stopping crawl right...');
+    stopCrawl('R');
+  };
 
   const handleBackStart = () => {
     console.log('Crawling backward...');
-    if (device) {
-      console.log('Transmitting command: 11');
-      const byteCommand = `Z${12}`;
-      BluetoothService.sendCommand(device,byteCommand); // Send the command to start crawling forward
-      setCrawlBackward(true); // Set the state to indicate crawling forward
-    }
+    startCrawl('Z');
   };
-
 
   const handleBackStop = () => {
-    console.log('Stopping crawl...');
-    if (device) {
-      console.log('Transmitting command: 8');
-      BluetoothService.sendCommand(device, new Uint8Array([19])); // Send the command to stop crawling
-      setCrawlBackward(false); // Set the state to indicate not crawling
-    }
+    console.log('Stopping crawl backward...');
+    stopCrawl('Z');
   };
 
-  const handleCrawlStart = () => {
+  const handleForwardStart = () => {
     console.log('Crawling forward...');
-    if (device) {
-      console.log('Transmitting command: 11');
-      const byteCommand = `F${11}`;
-      BluetoothService.sendCommand(device,byteCommand); // Send the command to start crawling forward
-      setCrawlForward(true); // Set the state to indicate crawling forward
-    }
+    startCrawl('F');
   };
 
   const handleForwardStop = () => {
-    console.log('Stopping crawl...');
-    if (device) {
-      console.log('Transmitting command: 14');
-      BluetoothService.sendCommand(device, new Uint8Array([19])); // Send the command to stop crawling
-      setCrawlForward(false); // Set the state to indicate not crawling
-    }
+    console.log('Stopping crawl forward...');
+    stopCrawl('F');
   };
-
-
 
   const handleStretch = () => {
-    console.log('Device:', device);
+    console.log('Stretching...');
     if (device) {
-      console.log('Transmitting command: S');
-      const byteCommand = `S${18}`;
-      BluetoothService.sendCommand(device,byteCommand ); // ASCII code for 'S'
+      BluetoothService.sendCommand(device, 'S');
     }
   };
-  const standLift = () => {
-    console.log('Device:', device);
-    if (device) {
-      const byteCommand = `U${13}`;
-      console.log('Transmitting command: U');
-      BluetoothService.sendCommand(device, byteCommand); // ASCII code for 'S'
-    }
-  };
-  const handleEyeballXChange = (value) => {
-    if (device) {
-      const byteCommand = `x${value}`; // Append "x" to indicate X-Axis
-      BluetoothService.sendCommand(device, byteCommand); // Send the command to adjust eyeball X-axis
-      console.log('Eyeball X-Axis:', byteCommand);
 
+  const handleMouseUp = () => {
+    console.log('Mouse released - executing lift and stretch...');
+    if (device) {
+      BluetoothService.sendCommand(device, 'U');
+      setTimeout(() => {
+        BluetoothService.sendCommand(device, 'S');
+      }, 500);
     }
   };
-  const handleEyeballYChange = (value) => {
-   if (device) {
-    const command = `y${value}`; // Construct the command string
-    BluetoothService.sendCommand(device, command); // Send the command as a string
-    console.log('Eyeball Y-Axis:', command);
-  }
-};
-  
-  const mouthDown = () => {
-    console.log('Device:', device);
+
+  const standLift = () => {
+    console.log('Lifting and standing...');
     if (device) {
-      console.log('Transmitting command: L');
-      BluetoothService.sendCommand(device, 20); // ASCII code for 'S'
+      BluetoothService.sendCommand(device, 'U');
+    }
+  };
+
+  const standUp = () => {
+    console.log('Standing...');
+    if (device) {
+      BluetoothService.sendCommand(device, 'T');
+    }
+  };
+
+  const mouthDown = () => {
+    console.log('Mouth down...');
+    if (device) {
+      BluetoothService.sendCommand(device, 'L20');
     }
   };
 
   const mouthUp = () => {
-    console.log('Device:', device);
+    console.log('Mouth up...');
     if (device) {
-      console.log('Transmitting command: L');
-      BluetoothService.sendCommand(device, 21); // ASCII code for 'S'
+      BluetoothService.sendCommand(device, 'L21');
     }
   };
-  const handleEyelidChange = (value) => {
-    console.log('Eyelid:', value);
+
+  const handleBend = () => {
+    console.log('Bending...');
     if (device) {
-      const command = `e${value}`; // Command format for adjusting eyelid position
-      BluetoothService.sendCommand(device, command); // Send the command to adjust eyelid position
+      BluetoothService.sendCommand(device, 'B');
     }
   };
-  const handleBend = (value) => {
-    console.log('Device:', device);
-    if (device) {
-      console.log('Transmitting command: Bend');
-      const command = `B${14}`; // Command format for adjusting eyelid position
-      BluetoothService.sendCommand(device, command); // ASCII code for 'S'
-    }
-  };
+
   const handleBlink = () => {
     console.log('Blinking...');
     if (device) {
-      console.log('Transmitting command: 16');
-      const command = `b${17}`; 
-      BluetoothService.sendCommand(device, command); // Send the command for blinking
+      BluetoothService.sendCommand(device, 'd');
     }
   };
 
-  const blinkDown = () => {
-    console.log('Button released');
-    if (device) {
-      console.log('Transmitting command: 15');
-      const command = `d${19}`;
-      BluetoothService.sendCommand(device, command );
-    }
-  };
-  const handleButtonUp = () => {
-    console.log('Button released');
-    if (device) {
-      console.log('Transmitting command: 15');
-      const command = `U${19}`;
-      BluetoothService.sendCommand(device, command );
-    }
+  const xBarDown = (e) => {
+    setDragging(true);
+    setStartX(e.clientX);
   };
 
-
-  const standUp = () => {
-    console.log('Button released');
-    if (device) {
-      console.log('Transmitting command: Stnd12');
-      const command = `T${10}`;
-      BluetoothService.sendCommand(device, command);
+  const xBarMove = (e) => {
+    if (dragging) {
+      const deltaX = e.clientX - startX;
+      const newWidth = width + deltaX;
+      const clampedWidth = Math.min(Math.max(newWidth, 20), 160);
+      setWidth(clampedWidth);
+      if (device) {
+        BluetoothService.sendCommand(device, `x${clampedWidth}`);
+      }
+      setStartX(e.clientX);
     }
   };
 
+  const xBarUp = () => {
+    setDragging(false);
+  };
+
+  const yBarDown = (e) => {
+    setDragging(true);
+    setStartY(e.clientY);
+  };
+
+  const yBarMove = (e) => {
+    if (dragging) {
+      const deltaY = e.clientY - startY;
+      const newHeight = height + deltaY;
+      const clampedHeight = Math.min(Math.max(newHeight, 20), 160);
+      setHeight(clampedHeight);
+      if (device) {
+        BluetoothService.sendCommand(device, `y${clampedHeight}`);
+      }
+      setStartY(e.clientY);
+    }
+  };
+
+  const yBarUp = () => {
+    setDragging(false);
+  };
 
   return (
     <div className="buttons">
-  <div className="eyeContainer">
-</div>
-<img src={eyeWindow} alt="rectangle" className="eyeWindow"/>
-<img src={mouthWindow} alt="rectangle" className="mouthWindow"/>
+      <div className="eyeContainer"></div>
+      <img src={eyeWindow} alt="rectangle" className="eyeWindow" />
+      <img src={mouthWindow} alt="rectangle" className="mouthWindow" />
+      <img src={FrontEyeBar} alt="rectangle" className="yFrontBar" />
+      <img src={FrontEyeBar} alt="rectangle" className="frontBar" />
 
-<img src={FrontEyeBar} alt="rectangle" className="frontBar"/>
-<img src={FrontEyeBar} alt="rectangle" className="xFrontBar"/>
       <div
-        className="rectangle"
+        className="xrectangle"
         style={{ width: `${width}px` }}
         onMouseDown={xBarDown}
         onMouseMove={xBarMove}
         onMouseUp={xBarUp}
         onMouseLeave={xBarUp}
-        min="10" max="160"
+        min="10"
+        max="160"
       >
-        <img src={eyeBar} alt="rectangle" className="rectangle-image" />
-
+        <img src={eyeBar} alt="rectangle" className="x-rectangle-image" />
       </div>
 
       <div
@@ -297,87 +207,92 @@ const handleRightStart = () =>{
         onMouseMove={yBarMove}
         onMouseUp={yBarUp}
         onMouseLeave={yBarUp}
-        min="10" max="160"
+        min="10"
+        max="160"
       >
         <img src={eyeBarCopy} alt="rectangle" className="y-rectangle-image" />
       </div>
 
-      
-         <div className="legs">
-      <button
-        onMouseDown={handleBackStart}
-        onMouseUp={handleBackStop}
-      >
-        Move Backward
-      </button>
-
-      <button
-        onMouseDown={handleCrawlStart}
-        onMouseUp={handleForwardStop}
-      >
-        Move Forward
-      </button>
-
-      <button
-        onMouseDown={handleRightStart}
-        onMouseUp={handleForwardStop}
-      >
-        Move Right
-      </button>
-
-
-
-
-      <button
-        onMouseDown={handleStretch}
-      >
-        Stretch
-      </button>
-      <button
-        onMouseDown={standLift}
-        onMouseUp={standUp}
-      >
-        Stand
-      </button>
-
-
-
-
-
-      <button
-        onMouseDown={handleBend}
-      >
-        Bend
-      </button>
-      </div>
-      <div className = "mouth">
+      <div className="legs">
         <button
-        onMouseDown={mouthDown}
-        onMouseUp={mouthUp}
-      >
-      Mouth
-      </button>
-      </div>
-      <div className='sliders'>
-      <div>
-        <label className= "xLabel" >X-Axis:</label>
-        <input className="xSlider" type="range" min="10" max="160" defaultValue="40" onChange={(e) => handleEyeballXChange(e.target.value)}/>
-      </div>
-      <div className = "eyeY">
-        <label className= "yLabel">Y-Axis:</label>
-        <input className="ySlider" type="range" min="10" max="120" defaultValue="40" onChange={(e) => handleEyeballYChange(e.target.value)}/>
-      </div>
-      <div className="eyelid">
-        <label className ="lidLabel">Eyelid:</label>
-        <input className="lidSlider" type="range" min="10" max="160" onChange={(e) => handleEyelidChange(e.target.value)}/>
-      </div>
-     <button className="blink" onMouseDown={handleBlink} onMouseUp={blinkDown}>
-  Blink
-</button>
+          className="backward_button"
+          onMouseDown={handleBackStart}
+          onMouseUp={handleBackStop}
+        >
+          <img
+            src={crawl_backward}
+            alt="rectangle"
+            className="crawlbackward"
+          />
+        </button>
 
-    </div>
+        <button
+          className="forward_button"
+          onMouseDown={handleForwardStart}
+          onMouseUp={handleForwardStop}
+        >
+          <img src={crawl_forward} alt="rectangle" className="crawlforward" />
+        </button>
+
+        <button
+          className="right_button"
+          onMouseDown={handleRightStart}
+          onMouseUp={handleRightStop}
+        >
+          <img src={crawl_right} alt="rectangle" className="crawlright" />
+        </button>
+
+        <button
+          className="left_button"
+          onMouseDown={handleLeftStart}
+          onMouseUp={handleLeftStop}
+        >
+          <img src={crawl_left} alt="rectangle" className="crawlleft" />
+        </button>
+      </div>
+
+      <div className="bend">
+        <button
+          className="bend_button"
+          onMouseDown={handleBend}
+          onMouseUp={handleMouseUp}
+        >
+          <img src={squat_down} alt="rectangle" className="bendbutton" />
+        </button>
+
+        <button
+          className="stand_button"
+          onMouseDown={standLift}
+          onMouseUp={standUp}
+        >
+          <img src={stand_image} alt="rectangle" className="standbutton" />
+        </button>
+
+        <button
+          className="mouth_button"
+          onMouseDown={mouthDown}
+          onMouseUp={mouthUp}
+        >
+          <img src={mouth} alt="rectangle" className="mouthbutton" />
+        </button>
+
+        <button className="stretch_button" onMouseDown={handleStretch}>
+          <img
+            src={stretch_image}
+            alt="rectangle"
+            className="stretcher"
+          />
+        </button>
+      </div>
+
+      <div className="eyebar"></div>
+      <img src={body_window} alt="rectangle" className="bodyWindow" />
+      <button className="blink_button" onClick={handleBlink}>
+        <img src={blink_button} alt="rectangle" className="blinkbutton" />
+      </button>
+      <div className="xrectangle"></div>
     </div>
   );
-  };
+};
 
 export default HexapodControl;
