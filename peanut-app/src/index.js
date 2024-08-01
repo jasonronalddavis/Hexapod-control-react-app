8cded4f456a6a2394c7e5952d8a688fa5bd8ae74
jasonronalddavis <jdavis@c.ringling.edu>
2024-07-31 20:08:29 -0500

    resolve warnings

--- peanut-app/src/index.js
+++ peanut-app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route} from 'react-dom/client';//import ListUsers from "../components/User/users";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
